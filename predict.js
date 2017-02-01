9f341e2cc7e8f2fb38ab6c1747ceae9aba9fa9fb
Jared Lerner <jared@clarifai.com>
2017-02-01 13:42:13 -0500

    Add new Alpha Models

--- predict.js
+++ predict.js
@@ -1,3 +1,9 @@
+/*
+  Purpose: Pass information to other helper functions after a user clicks 'Predict'
+  Args:
+  	value - Actual filename or URL
+  	source - 'url' or 'file'
+*/
 function predict_click(value, source) {
   
   if(source == 'url') {
@@ -27,33 +33,111 @@ function predict_click(value, source) {
   } 
 }
 
+/*
+  Purpose: Does a v2 prediction based on user input
+  Args:
+  	value - Either {url : url_value} or { base64 : base64_value }
+*/
 function doPredict(value) {
-  
-  app.models.predict(getSelectedModel(), value).then(
+
+  var model_id = getSelectedModel();
+
+  app.models.predict(model_id, value).then(
     
     function(response) {
       let concept_names = "";
-      var tag_array;
-    
-      // Check for color model since it has different JSON
-      if(getSelectedModel() != Clarifai.COLOR_MODEL) {
+      var tag_array, people_array;
+      var tag_count = 0;
+      var model_name = response.rawData.outputs[0].model.name;
+      
+      // Check for A/G/E, Faces and Celebrity First 
+      // They all look for people
+      if(model_name == "age_gender_ethnicity" || model_name == "face-v1.3" || model_name == "celeb-v1.3") {
+      	var people_array = response.rawData.outputs[0].data.regions;
+      	console.log(response);
+      	
+      	// First check if there any faces to evaluate
+      	if(people_array == null) {
+      		$('#concepts').html("<br/><br/><b>No Faces Detected!</b>");
+      		return;
+      	}
+      	
+      	// faces are found, so iterate through all of them
+      	for(let i = 0; i < people_array.length; i++) {
+      		concept_names += '<b>Person ' + (i+1) + '</b>';  
+      		 		
+      		// A/G/E has separate sub-arrays
+      		if(model_name == "age_gender_ethnicity") {
+      			age_array = people_array[i].data.faces[0].age;
+      			ethnic_array = people_array[i].data.faces[0].ethnicity;
+      			gender = people_array[i].data.faces[0].gender[0];
+      		
+      			// Age Header
+      			concept_names += '<br/><b><span style="font-size:10px">Age</span></b>';
+      		
+      			// print top 5 ages
+      			for(let a = 0; a < 5; a++)
+      				concept_names += '<li>' + age_array[a].name + ': <i>' + age_array[a].value + '</i></li>'; 
+      		
+      			// Ethnicity Header
+      			concept_names += '<b><span style="font-size:10px">Ethnicity</span></b>'
+      			
+      			// print top 3 ethnicities
+      			for(let e = 0; e < 3; e++)
+      				concept_names += '<li>' + ethnic_array[e].name + ': <i>' + ethnic_array[e].value + '</i></li>'; 
+      		      		
+      			// Gender Header
+      			concept_names += '<b><span style="font-size:10px">Gender</span></b>'
+      		
+      			// print gender
+      			concept_names += '<li>' + gender.name + ': <i>' + gender.value + '</i></li>'; 
+      		}
+      		
+      		// For faces just print bounding boxes
+      		else if(model_name == "face-v1.3") {
+      			// Top Row
+      			concept_names += '<li>Top Row: <i>' + people_array[i].region_info.bounding_box.top_row + '</i></li>';
+      			concept_names += '<li>Left Column: <i>' + people_array[i].region_info.bounding_box.left_col + '</i></li>';
+      			concept_names += '<li>Bottom Row: <i>' + people_array[i].region_info.bounding_box.bottom_row + '</i></li>';
+      			concept_names += '<li>Right Column: <i>' + people_array[i].region_info.bounding_box.right_col + '</i></li>';
+      		}
+      		
+      		// Celebrity
+      		else {
+      			tag_array = people_array[i].data.faces[0].identity;
+      			
+      			// Print first 10 results
+      			for(var c=0; c < 10; c++)
+      				concept_names += '<li>' + tag_array[c].name + ': <i>' + tag_array[c].value + '</i></li>'; 
+      		}
+      		
+      		tag_count+=10;      	
+     		}
+     	}
+      
+      // Check for color model since it has its own unique JSON
+      else if(model_name != "color") {
         tag_array = response.rawData.outputs[0].data.concepts;
- 
+        
         for (let i = 0; i < tag_array.length; i++) 
           concept_names += '<li>' + tag_array[i].name + ': <i>' + tag_array[i].value + '</i></li>';
+          
+        tag_count=tag_array.length;
       }
+      
+      // Color Model
       else {
-        tag_array = response.data.outputs[0].data.colors;
-        for (let i = 0; i < tag_array.length; i++)
-          concept_names += '<li>' + tag_array[i].w3c.name + ': <i>' + tag_array[i].value + '</i></li>';
+        tag_array = response.rawData.outputs[0].data.colors;
+        
+        for (let col = 0; col < tag_array.length; col++)
+          concept_names += '<li>' + tag_array[col].w3c.name + ': <i>' + tag_array[col].value + '</i></li>';
+
+        tag_count=tag_array.length;
       }
       
-      // show tags in 2 columns if there are more than 10. Else show 1
-      if(tag_array.length > 10)
-        concept_names = '<ul style="margin-right:20px; margin-top:20px; columns:2; -webkit-columns:2; -moz-columns:2;">' + concept_names;
+      var column_count = tag_count / 10;
       
-      else
-        concept_names = '<ul style="margin-right:20px; margin-top:20px;">' + concept_names;
+      concept_names = '<ul style="margin-right:20px; margin-top:20px; columns:' + column_count + '; -webkit-columns:' + column_count + '; -moz-columns:' + column_count + ';">' + concept_names;
         
       concept_names += '</ul>';
       $('#concepts').html(concept_names);
@@ -66,6 +150,11 @@ function doPredict(value) {
   );
 }
 
+/*
+  Purpose: Return a back-end model id based on current user selection
+  Returns:
+    Back-end model id
+*/
 function getSelectedModel() {
   var model = document.querySelector('input[name = "model"]:checked').value;
   
@@ -91,17 +180,25 @@ function getSelectedModel() {
     return "e0be3b9d6a454f0493ac3a30784001ff";
     
   else if(model == "faces")
-    return "c67b5872d8b44df4be55f2b3de3ebcbb";
+    return "a403429f2ddf4b49b307e318f00e528b";
   
   else if(model == "demographic")
     return "c0c0ac362b03416da06ab3fa36fb58e3";
     
+  else if(model == "celebrity")
+  	return "e466caa0619f444ab97497640cefc4dc";
+  
   else if(model == "custom") {
     var e = document.getElementById("custom_models_dropdown");
     return e.options[e.selectedIndex].value;
   }
 }
 
+/*
+  Purpose: Add an image to an application after user clicks button
+  Args:
+    index - # of the image in the session
+*/
 function addImageToApp() {
   var img_type = document.getElementById("hidden-type").value;
   var img_value = document.getElementById("hidden-val").value;
